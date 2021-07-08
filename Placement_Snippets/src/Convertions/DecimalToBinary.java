0cad94ec2a608e18cc9d71cfe9f25aa02e4bcec2
dixitabhishek239 <60729566+dixitabhishek239@users.noreply.github.com>
2021-07-08 15:19:43 +0530

    Decimal to Binary

--- Placement_Snippets/src/Convertions/DecimalToBinary.java
+++ Placement_Snippets/src/Convertions/DecimalToBinary.java
@@ -1,6 +1,39 @@
 package Convertions;
 
+import java.util.ArrayList;
+
 public class DecimalToBinary {
+	
+	Integer decimalNumber;
+	String binaryNumber;
+
+	public DecimalToBinary(int decimalNumber) {
+
+		this.decimalNumber = decimalNumber;
+		
+		int number = this.decimalNumber;
+		int quotient = 0;
+		int reminder = 0;
+		ArrayList<Integer> answer = new ArrayList<Integer>(); 
+		
+		
+		do {
+			
+			quotient = number/2;
+			reminder = number%2;
+			number = quotient;
+			
+//			System.out.print(number+ "  " + quotient + "  " + reminder);
+			
+			answer.add(reminder);
+			
+		}while(quotient!=0);
+		
+		for(int i=0; i<answer.size(); i++) {
+			System.out.print(answer.get(answer.size()-i-1));
+		}
+		
+	}
 
 	
 	
--- Placement_Snippets/src/Convertions/mainClass.java
+++ Placement_Snippets/src/Convertions/mainClass.java
@@ -10,11 +10,16 @@ public class mainClass {
 
 		System.out.println("Number Converstion System");
 		
-		System.out.println("Binary to Decimal");
-		System.out.print("Binary Number - ");
-		int binaryNumber = scanner.nextInt();
-		
-		new BinaryToDecimal(binaryNumber);
+//		System.out.println("Binary to Decimal");
+//		System.out.print("Binary Number - ");
+//		int binaryNumber = scanner.nextInt();
+//		new BinaryToDecimal(binaryNumber);
+	
+		System.out.println("Decimal to Binary");
+		System.out.print("Decimal Number - ");
+		int decimalNumber = scanner.nextInt();
+		new DecimalToBinary(decimalNumber);
+
 		
 	}
 
